83319d12a75164909313abff3007a84f68b8090f
Steffen Daode Nurpmeso <sdaoden@gmail.com>
2011-11-08 16:16:37 +0100

    openbsd-changelog-plus.pl: correct URL encoding and *all* catches

--- openbsd-changelog-plus.pl
+++ openbsd-changelog-plus.pl
@@ -1,52 +1,62 @@
 #!/usr/bin/perl
 #@ Simple thing to maintain entries of OpenBSD plus.html
+#@ - Blank lines are ignored
+#@ - First non-blank line must consist only of <!-- YYYY/MM/DD -->
+#@ - Any other such line starts a new day
+#@ - All remaining lines are converted to <li> lines in FILO order
+#@ - Links are produced for name(section) and name(section/arch);
+#@   name(!section) and name(!section/arch) are not converted, but the
+#@   exclamation mark is simply removed.
+#@ - The links which are produced are percent-escaped rather acc. to RFC 3986,
+#@   and their style is identical to OpenBSD man page content (i.e. archive is
+#@   lowercase)
 
 my $WEB = 'http://www.openbsd.org/cgi-bin/man.cgi';
 my $manre = '[][+.:\w-]+';
+
 my @days;
 
 while (<STDIN>) {
     chomp;
-    next if /^\s*$/;
-    if ($_ =~ /^<!--\s+\d{4}\/\d{2}\/\d{2}\s+-->/) {
+    s/^\s*(.*?)\s*$/$1/g;   # Remove leading + trailing WS
+    next if /^\s*$/;        # Ign empty
+
+    # Start a new day?
+    if ($_ =~ /^<!--\s+\d{4}\/\d{2}\/\d{2}\s+-->/o) {
         my @new_entries;
         push @new_entries, $_;
         push @days, \@new_entries;
         next;
     }
-    die "First line must contain <!-- YYYY/MM/DD --> tag" unless @days;
 
-    my ($l, $w) = $_;
-    $l =~ s/^(\w)/<li>$1/;
+    die "First non-blank line must EQ <!-- YYYY/MM/DD -->" unless @days;
+    my ($l, $w) = '<li>' . $_;
 
     # abc(4)
     $w = '';
-    while (defined $l && $l =~ /(.*?)($manre)\((\d)\)(.*)/) {
+    while ($l =~ /(.*?)($manre)\((\d)\)(.*)/o) {
         $l = $4;
         $w .= $1 if defined $1;
         my ($q, $s, $ql) = ($2, $3);
-        ($ql = $q) =~ s/\[/%5B/g;
-        $ql =~ s/\+/%2B/g;
+        ($ql = $q) =~ s/([^\w()'*~!.-])/sprintf '%%%02X', ord $1/eg;
         $w .= "<a href=\"$WEB?query=$ql&sektion=$s&format=html\">$q($s)</a>";
     }
     $l = "$w$l" if length $w;
 
     # abc(4/amd64)
     $w = '';
-    while (defined $l && $l =~ /(.*?)($manre)\((\d)\/(\w+)\)(.*)/) {
+    while ($l =~ /(.*?)($manre)\((\d)\/(\w+)\)(.*)/o) {
         $l = $5;
         $w .= $1 if defined $1;
-        my ($q, $s, $au, $al, $ql) = ($2, $3, $4);
-        ($ql = $q) =~ s/\[/%5B/g;
-        $ql =~ s/\+/%2B/g;
-        $al = lc $au;
-        $au = uc $au;
-        $w .= "<a href=\"$WEB?query=$ql&sektion=$s&arch=$al&format=html\">$q($s/$au)</a>";
+        my ($q, $s, $a, $ql) = ($2, $3, $4);
+        ($ql = $q) =~ s/([^\w()'*~!.-])/sprintf '%%%02X', ord $1/eg;
+        $a = lc $a;
+        $w .= "<a href=\"$WEB?query=$ql&sektion=$s&arch=$a&format=html\">$q($s/$a)</a>";
     }
     $l = "$w$l" if length $w;
 
     # abc(!4) is not expanded to link but to plain abc(4)
-    $l =~ s/($manre)\(!(\d)\)/$1($2)/g;
+    $l =~ s/($manre)\(!(\d(?:\/\w+)?)\)/$1(\L$2\E)/og;
     push @{$days[@days - 1]}, $l;
 }
 
