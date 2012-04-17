018bdc7367e6a4046e59d8f70c19ab6808eb1a6b
Steffen Daode Nurpmeso <sdaoden@gmail.com>
2012-04-17 18:12:48 +0200

    showkey.c, v8: BUGFIX Linux for good, plus..
    Actually a faulty busybox bugreport of mine (SHAME!)
    made me look a bit at Linux for real.
    
    It was plain wrong yet, since K_MEDIUMRAW must be used to
    actually get at the real keycode, K_RAW gives the scancodes but
    which cannot be used to calculate the keycode.
    
    So reorganize and reorder the entire program.
    It now supports three different modes of operation:
    
    (1) value.  Much better than before, this prints the real input
        string that could also be obtained by pressing the key when
        a cat(1) is active, plus the values as before, but using
        a line for each input character.
        This is already supported on all platforms, as it is
        a generic implementation.
    (2) scancodes.  This prints the raw scancodes from the keyboard.
        This mode of operation should also be fine per se, but for
        now scancode=keycode for FreeBSD, which is most likely
        wrong.
    (3) keycodes.  For Linux there is a special mode for this.
        On OpenBSD this is equal to the scancode.
        On FreeBSD we yet go the same approach as for OpenBSD, but
        this is most likely wrong since FreeBSD offers K_CODE and
        '$ kbdcontrol -d' gives something that looks more like Linux
        keycodes.
        But i don't know yet - for now, print a big fat warning if
        a FreeBSD user wants to get keycodes; i'll surely find some
        time during this year, and version 10 of showkey.c will
        support all the mentioned architectures just fine.
    
        By the way: the wscons stuff from OpenBSD (and NetBSD) also
        has some pitfalls still, in conjunction with NumLock and
        such.
        I think i'll have to take a full kernel keyboard walk to
        understand the kernel side before showkey.c will work
        gracefully.
    
        I'm not convinced that this should be necessary for a user
        to be able to understand keycodes and scancodes and handle
        all possible occurrences thereof.

--- showkey.c
+++ showkey.c
@@ -1,6 +1,12 @@
-/*@ showkey.c v7: show keyboard scancodes for FreeBSD, OpenBSD, NetBSD, Linux.
+/*@ showkey.c v8: show keyboard scancodes for FreeBSD, OpenBSD, (NetBSD), Linux.
+ *@ - Linux should be completely correct since v8.
+ *@ - OpenBSD should work fine except for some; you can always compare against
+ *@   '$ wsconsctl keyboard.map' for problems with NumLock key combinations+.
+ *@   TODO I need to take a very deep look to fix the rest of OpenBSD.
+ *@ - TODO FreeBSD: K_CODE mode isn't supported at all, FIXME keycodes wrong!?!
+ *@ - TODO I've no access to NetBSD, but assume the same approach as OpenBSD.
  *@ Compile     : $ gcc -W -Wall -pedantic -o showkey showkey.c
- *@ Run         : $ ./showkey [ktv]  (keycode, termios seq., termios vals)
+ *@ Run         : $ ./showkey [ksv]  (keycodes, scancodes, values)
  *@ Exit status : 0=timeout, 1=signal/read error, 3=use/setup failure
  *
  * Copyright (c) 2012 Steffen Daode Nurpmeso <sdaoden@users.sourceforge.net>.
@@ -32,7 +38,7 @@
  */
 
 /* How many seconds of inactivity before program terminates? */
-#define ALARM_DELAY     5
+#define ALARM_DELAY     9
 
 /**  >8  **  8<  **/
 
@@ -62,48 +68,64 @@
 # error Operating system not supported
 #endif
 
-static int              tios_only, caught_sig;
+enum operation {
+    op_SCANCODE,
+#ifdef USE_LINUX
+    op_KEYCODE,
+#else
+    op_KEYCODE  = op_SCANCODE,
+#endif
+    op_VALUE
+};
+
+static enum operation   op_mode;
+static int              caught_sig;
 static struct termios   tios_orig, tios_raw;
 
 /* Signal handler, exit 0 if SIGALRM, 1 otherwise */
 static void     onsig(int sig);
 
-/* Modes which handle input */
-static ssize_t  mode_term(unsigned char *buf, ssize_t len),
-                mode_value(unsigned char *buf, ssize_t len),
-                mode_keycode(unsigned char *buf, ssize_t len);
-
 /* Terminal handling */
 static void     raw_init(void), raw_on(void), raw_off(void);
+
+/* Actual modes */
+static ssize_t  mode_value(unsigned char *buf, ssize_t len),
+#ifdef USE_LINUX
+                mode_keycode(unsigned char *buf, ssize_t len),
+#else
+# define        mode_keycode mode_scancode
+#endif
+                mode_scancode(unsigned char *buf, ssize_t len);
 
 int
 main(int argc, char **argv)
 {
-    ssize_t (*mode)(unsigned char*, ssize_t) = &mode_keycode;
-    ssize_t i, skip;
     auto struct sigaction sa;
     auto struct itimerval it;
     auto unsigned char buf[64];
+    ssize_t (*mode)(unsigned char*, ssize_t);
+    ssize_t i, skip;
 
     if (argc > 1)
         switch (**++argv) {
         case 'k':
+            op_mode = op_KEYCODE;
             mode = &mode_keycode;
             break;
-        case 't':
-            mode = &mode_term;
-            tios_only = 1;
+        case 's':
+            op_mode = op_SCANCODE;
+            mode = &mode_scancode;
             break;
         case 'v':
+            op_mode = op_VALUE;
             mode = &mode_value;
-            tios_only = 1;
             break;
         default:
             mode = NULL;
             break;
         }
     if (mode == NULL)
-        errx(3, "Usage: showkey [ktv]  (keycode, termios, value)");
+        errx(3, "Usage: showkey [ksv]  (keycode, scancode, value)");
 
     if (! isatty(STDIN_FILENO))
         err(3, "STDIN is not a terminal");
@@ -121,6 +143,7 @@ main(int argc, char **argv)
     it.it_interval.tv_sec = it.it_interval.tv_usec = 0;
 
     raw_on();
+
     printf("You may now use the keyboard;\r\n"
         "After %d seconds of inactivity the program terminates\r\n",
         ALARM_DELAY);
@@ -151,37 +174,155 @@ onsig(int sig)
     return;
 }
 
-static ssize_t
-mode_term(unsigned char *buf, ssize_t len)
+static void
+raw_init(void)
 {
-    if (*buf == 0x1B)
-        while (--len >= 0) {
-            printf("0x%02X ", (unsigned int)*buf++);
-            if (len > 0 && *buf == 0x1B) /* XXX May be incomplete seq.?? */
-                printf("\r\n");
-        }
-    else
-        while (--len >= 0)
-            printf("0x%02X ", (unsigned int)*buf++);
-    printf("\r\n");
+    if (tcgetattr(STDIN_FILENO, &tios_orig) < 0)
+        err(3, "Can't query terminal attributes");
 
-    return (0);
+    tios_raw = tios_orig;
+    if (op_mode != op_VALUE)
+        (void)cfmakeraw(&tios_raw);
+    else {
+        tios_raw.c_lflag &= ~(ICANON | ISIG);
+        tios_raw.c_lflag |= ECHO | ECHOCTL;
+        tios_raw.c_iflag = 0;
+        tios_raw.c_cc[VMIN] = 1;
+        tios_raw.c_cc[VTIME] = 0;
+    }
+
+    return;
 }
+
+#if defined USE_SYSCONS || defined USE_LINUX
+static int  kbmode_raw, kbmode_orig;
+
+static void
+raw_on(void)
+{
+# ifdef USE_LINUX
+    kbmode_raw = (op_mode == op_KEYCODE) ? K_MEDIUMRAW : K_RAW;
+# else
+    kbmode_raw = K_RAW; /* TODO K_CODE support on FreeBSD */
+    if (op_mode == op_KEYCODE) /* FIXME FreeBSD */
+        fprintf(stderr, "WARNING: FreeBSD keycodes are most likely wrong\r\n"
+            "WARNING: the scancodes (in parenthesis) are fine\r\n");
+# endif
+
+    if (op_mode != op_VALUE &&
+            ioctl(STDIN_FILENO, KDGKBMODE, (long)&kbmode_orig) < 0)
+        err(3, "Can't query keyboard mode (on a pty?)");
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &tios_raw) < 0)
+        err(3, "Can't set terminal attributes");
+
+    if (op_mode != op_VALUE &&
+            ioctl(STDIN_FILENO, KDSKBMODE, (long)kbmode_raw) < 0) {
+        int sverr = errno;
+        (void)tcsetattr(STDIN_FILENO, TCSAFLUSH, &tios_orig);
+        /* Used to test for ENOTTY, but Linux seems not to use it.
+         * So re-set errno and simplify error message */
+        errno = sverr;
+        err(3, "Can't set keyboard mode (on a pty?)");
+    }
+
+    return;
+}
+
+static void
+raw_off(void)
+{
+    if (op_mode != op_VALUE)
+        (void)ioctl(STDIN_FILENO,  KDSKBMODE, (long)kbmode_orig);
+
+    (void)tcsetattr(STDIN_FILENO, TCSAFLUSH, &tios_orig);
+    return;
+}
+
+#endif /* USE_SYSCONS || USE_LINUX */
+#ifdef USE_WSCONS
+
+static void
+raw_on(void)
+{
+    auto int arg = WSKBD_RAW;
+
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &tios_raw) < 0)
+        err(3, "Can't set terminal attributes");
+
+    if (op_mode != op_VALUE && ioctl(STDIN_FILENO, WSKBDIO_SETMODE, &arg) < 0) {
+        arg = errno;
+        (void)tcsetattr(STDIN_FILENO, TCSANOW, &tios_orig);
+        /* Mode won't work on pseudo terminals and needs
+         * WSDISPLAY_COMPAT_RAWKBD kernel option */
+        errno = arg;
+        err(3, "Can't set keyboard mode (on a pty?)");
+    }
+
+    return;
+}
+
+static void
+raw_off(void)
+{
+    auto int arg = WSKBD_TRANSLATED;
+
+    if (op_mode != op_VALUE)
+        (void)ioctl(STDIN_FILENO, WSKBDIO_SETMODE, &arg);
+
+    (void)tcsetattr(STDIN_FILENO, TCSANOW, &tios_orig);
+    return;
+}
+
+#endif /* USE_WSCONS */
 
 static ssize_t
 mode_value(unsigned char *buf, ssize_t len)
 {
     while (--len >= 0) {
         unsigned int v = (unsigned int)*buf++;
-        printf("%4d 0%03o 0x%02X  ", v, v, v);
+        printf("\t%4d 0%03o 0x%02X\r\n", v, v, v);
     }
-    printf("\r\n");
 
     return (0);
 }
 
+#ifdef USE_LINUX
+
 static ssize_t
 mode_keycode(unsigned char *buf, ssize_t len)
+{
+    unsigned char *cursor;
+
+    for (cursor = buf; --len >= 0;) {
+        unsigned int kc = *cursor++, rc = kc & 0x7F;
+
+        if (rc == 0) {
+            if (len < 2) {
+                buf[0] = (unsigned char)kc;
+                if (++len == 2)
+                    buf[1] = *cursor;
+                break;
+            }
+
+            if ((cursor[0] & 0x80) != 0 && (cursor[1] & 0x80) != 0) {
+                rc  = *(cursor++) & 0x7F;
+                rc <<= 7;
+                rc |= *(cursor++) & 0x7F;
+                len -= 2;
+            }
+        }
+
+        printf("keycode %3u %s\r\n", rc, ((kc & 0x80) ? "release" : "press"));
+    }
+
+    return ((len <= 0) ? 0 : len);
+}
+
+#endif /* USE_LINUX */
+
+static ssize_t
+mode_scancode(unsigned char *buf, ssize_t len)
 {
     unsigned char *cursor;
 
@@ -204,98 +345,32 @@ mode_keycode(unsigned char *buf, ssize_t len)
 
         isdown = (0 == (kc & 0x80));
         kc &= ~0x0080;
-        if (! isplain)
-            kc |= 0x80;
 
-        printf("keycode %3u %-7s (0x%04X: 0x%04X | %c)\r\n",
-            kc, (isdown ? "press" : "release"),
-            rc, kc, (isdown ? 'v' : '^'));
+        if (isplain) {
+#ifndef USE_LINUX
+            printf("keycode %3u %-7s (     0x%02X: 0x%02X  | %c)\r\n",
+                kc, (isdown ? "press" : "release"),
+                rc, kc, (isdown ? 'v' : '^'));
+#else
+            printf("scancode      0x%02X (0x%04X | %s)\r\n",
+                (rc & 0xFF), kc, (isdown ? "press" : "release"));
+#endif
+        } else {
+            kc |= 0x80;
+#ifndef USE_LINUX
+            printf("keycode %3u %-7s (0x%02X 0x%02X: 0x%-3X | %c)\r\n",
+                kc, (isdown ? "press" : "release"),
+                (rc & 0xFF00) >> 8, (rc & 0xFF),
+                kc, (isdown ? 'v' : '^'));
+#else
+            printf("scancode 0x%02X 0x%02X (0x%04X | %s)\r\n",
+                (rc & 0xFF00) >> 8, (rc & 0xFF),
+                kc, (isdown ? "press" : "release"));
+#endif
+        }
     }
 
     return ((len <= 0) ? 0 : len);
 }
-
-#if defined USE_SYSCONS || defined USE_LINUX
-
-static void
-raw_init(void)
-{
-    if (tcgetattr(STDIN_FILENO, &tios_orig) < 0)
-        err(3, "Can't query terminal attributes");
-    tios_raw = tios_orig;
-    (void)cfmakeraw(&tios_raw);
-    return;
-}
-
-static void
-raw_on(void)
-{
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &tios_raw) < 0)
-        err(3, "Can't set terminal attributes");
-
-    if (! tios_only && ioctl(STDIN_FILENO, KDSKBMODE, (long)K_RAW) < 0) {
-        int sverr = errno;
-        (void)tcsetattr(STDIN_FILENO, TCSANOW, &tios_orig);
-        /* Used to test for ENOTTY, but Linux seems not to use it.
-         * So re-set errno and simplify error message */
-        errno = sverr;
-        err(3, "Can't initialize keyboard (pseudo terminal?)");
-    }
-    return;
-}
-
-static void
-raw_off(void)
-{
-    if (! tios_only)
-        (void)ioctl(STDIN_FILENO,  KDSKBMODE, (long)K_XLATE);
-    (void)tcsetattr(STDIN_FILENO, TCSAFLUSH, &tios_orig);
-    return;
-}
-
-#endif /* USE_SYSCONS || USE_LINUX */
-#ifdef USE_WSCONS
-
-static void
-raw_init(void)
-{
-    if (tcgetattr(STDIN_FILENO, &tios_orig) < 0)
-        err(3, "Can't query terminal attributes");
-    tios_raw = tios_orig;
-    (void)cfmakeraw(&tios_raw);
-    return;
-}
-
-static void
-raw_on(void)
-{
-    auto int arg = WSKBD_RAW;
-
-    if (tcsetattr(STDIN_FILENO, TCSANOW, &tios_raw) < 0)
-        err(3, "Can't set terminal attributes");
-
-    if (! tios_only && ioctl(STDIN_FILENO, WSKBDIO_SETMODE, &arg) < 0) {
-        arg = errno;
-        (void)tcsetattr(STDIN_FILENO, TCSANOW, &tios_orig);
-        /* Mode won't work on pseudo terminals and needs
-         * WSDISPLAY_COMPAT_RAWKBD kernel option */
-        errno = arg;
-        err(3, "Can't initialize keyboard (pseudo terminal?)");
-    }
-    return;
-}
-
-static void
-raw_off(void)
-{
-    auto int arg = WSKBD_TRANSLATED;
-
-    if (! tios_only)
-        (void)ioctl(STDIN_FILENO, WSKBDIO_SETMODE, &arg);
-    (void)tcsetattr(STDIN_FILENO, TCSANOW, &tios_orig);
-    return;
-}
-
-#endif /* USE_WSCONS */
 
 /* vim:set fenc=utf-8 filetype=c syntax=c ts=4 sts=4 sw=4 et tw=79: */
