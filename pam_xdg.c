8f787c615f35abf976928254f3cc68fdedfabbe5
Steffen Nurpmeso <steffen@sdaoden.eu>
2021-07-30 14:20:28 +0200

    pam_xdg: FIX security; fix doc mismatch; port to OpenPAM (BSD) (Jan Bleich)..
    - If the "user" in /run/user did not exist we created it.
      For this we temporarily setegid(0) and another umask.
      It happens that if we could not create "user" we directly
      failed, without restoring the original egid and umask.
    
    - Manual said "rundir" but it always was "runtime".
      Temporarily allow both.
    
    - Port to OpenPAM that is used on the BSDs.
      For this we now are capable to create the entire /run/user path,
      since /run is not normally existing on BSD.  (Jan Bleich)

--- pam_xdg.c
+++ pam_xdg.c
@@ -1,12 +1,8 @@
 /*@ pam_xdg - manage XDG Base Directories (runtime dir life time, environment).
- *@ Create /run/user/`id -u` when the first session is opened.
- *@ It also creates according XDG_RUNTIME_DIR etc. environment variables in the
- *@ user sessions, except when given the "runtime" option, in which case it
- *@ only creates XDG_RUNTIME_DIR and not the others.
- *@ Place for example in /etc/pam.d/common-session one of the following:
- *@   session options pam_xdg.so [runtime] [notroot]
- *@ Notes: - according to XDG Base Directory Specification, v0.7.
- *@        - Linux-only (i think).
+ *@ See pam_xdg.8 for more.
+ *@ - According to XDG Base Directory Specification, v0.7.
+ *@ - Supports libpam (Linux) and *BSD OpenPAM.
+ *@ - Requires C preprocessor with __VA_ARGS__ support!
  *
  * Copyright (c) 2021 Steffen Nurpmeso <steffen@sdaoden.eu>.
  * SPDX-License-Identifier: ISC
@@ -32,11 +28,10 @@
 #define a_XDG_CONFIG_DIRS_DEF "/etc/xdg/"
 #define a_XDG_CACHE_HOME_DEF "\1/.cache"
 
-/* */
-#define a_XDG "pam_xdg"
-
-#define a_RUNTIME_DIR_OUTER "/run" /* This must exist already */
-#define a_RUNTIME_DIR_BASE "user" /* We create this as necessary, thus. */
+/* We create the outer directories as necessary (stack buffer storage!) */
+#define a_RUNTIME_DIR_OUTER "/run"
+#define a_RUNTIME_DIR_OUTER_MODE 0755
+#define a_RUNTIME_DIR_BASE "user"
 #define a_RUNTIME_DIR_BASE_MODE 0755 /* 0711? */
 
 /* >8 -- 8< */
@@ -52,57 +47,141 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <syslog.h>
 #include <unistd.h>
 
+#include <security/pam_appl.h>
 #include <security/pam_modules.h>
-#include <security/pam_ext.h>
+#ifdef OPENPAM_VERSION
+# include <security/openpam.h>
+#else
+# include <security/pam_ext.h>
+#endif
+
+#ifdef OPENPAM_VERSION
+#else
+# include <syslog.h>
+#endif
+
+/* Who are we? */
+#define a_XDG "pam_xdg"
 
 /* _XOPEN_PATH_MAX POSIX 2008/Cor 1-2013 */
 #ifndef PATH_MAX
 # define PATH_MAX 1024
 #endif
 
+/* */
+#ifdef O_SEARCH
+# define a_O_SEARCH O_SEARCH
+#elif defined O_PATH
+# define a_O_SEARCH O_PATH
+#else
+  /* Well, hardly, but not in practice so do not #error out */
+# define a_O_SEARCH 0
+#endif
+
+/* libpam / OpenPAM compat */
+#ifdef OPENPAM_VERSION
+# define a_LOG(HDL, LVL, ...) ((void)HDL, openpam_log(LVL, __VA_ARGS__))
+# define a_LOG_ERR PAM_LOG_ERROR
+# define a_LOG_NOTICE PAM_LOG_NOTICE
+#else
+# define a_LOG(HDL, LVL, ...) pam_syslog(HDL, LVL, __VA_ARGS__)
+# define a_LOG_ERR LOG_ERR
+# define a_LOG_NOTICE LOG_NOTICE
+#endif
+
+/* Because of complicated file locking, use one function with two exec paths */
 static int a_xdg(int isopen, pam_handle_t *pamh, int flags, int argc,
       const char **argv);
 
 static int
 a_xdg(int isopen, pam_handle_t *pamh, int flags, int argc, const char **argv){
-   char uidbuf[sizeof "18446744073709551615"],
+   enum a_flags{
+      a_NONE,
+      /* Options */
+      a_RUNTIME = 1u<<0,
+      a_NOTROOT = 1u<<1,
+#if 0
+      a_SESSIONS = 1u<<16,
+      a_USER_LOCK = 1u<<17,
+#endif
+      /* Flags */
+      a_MPV = 1u<<29, /* Multi-Purpose-Vehicle */
+      a_SKIP_XDG = 1u<<30 /* We shall not act */
+   };
+
+   struct a_dirtree{
+      char const *name;
+      int mode;
+   };
+
+   static struct a_dirtree const a_dirtree[] = {
+      {a_RUNTIME_DIR_OUTER, a_RUNTIME_DIR_OUTER_MODE},
+      {a_RUNTIME_DIR_BASE, a_RUNTIME_DIR_BASE_MODE},
+      {NULL, 0} /* XXX -> nelem/item/countof */
+   };
+   static int f_saved;
+
+   char uidbuf[sizeof ".18446744073709551615"],
          wbuf[((sizeof("XDG_RUNTIME_DIR=") + sizeof(a_RUNTIME_DIR_OUTER) +
-               sizeof(a_RUNTIME_DIR_BASE) + sizeof("18446744073709551615")) |
+               sizeof(a_RUNTIME_DIR_BASE) + sizeof(".18446744073709551615")) |
             (sizeof("XDG_CONFIG_DIRS=") + PATH_MAX)
             ) +1];
-   struct stat st;
+   struct a_dirtree const *dtp;
    struct passwd *pwp;
    char const *emsg;
-   int cwdfd, only_runtime, notroot, res, uidbuflen;
+   int cwdfd, f, res, uidbuflen;
    char const *user;
-   (void)flags;
 
    user = "<unset>";
-   cwdfd = -1;
-   only_runtime = notroot = 0;
+   cwdfd = AT_FDCWD;
 
    /* Command line */
    if(isopen){
+      f = a_NONE;
+
       for(; argc > 0; ++argv, --argc){
          if(!strcmp(argv[0], "runtime"))
-            only_runtime = 1;
+            f |= a_RUNTIME;
+         else if(!strcmp(argv[0], "rundir")){ /* XXX COMPAT */
+            a_LOG(pamh, a_LOG_NOTICE,
+               a_XDG ": \"rundir\" was a misdocumentation of \"runtime\", "
+               "sorry for this");
+            f |= a_RUNTIME;
+         }
          else if(!strcmp(argv[0], "notroot"))
-            notroot = 1;
+            f |= a_NOTROOT;
+#if 0
+         else if(!strcmp(argv[0], "track_user_sessions"))
+            f |= a_SESSIONS;
+         else if(!strcmp(argv[0], "per_user_lock"))
+            f |= a_USER_LOCK;
+#endif
          else if(!(flags & PAM_SILENT)){
             emsg = "command line";
             errno = EINVAL;
             goto jerr;
          }
       }
-   }else
+
+#if 0
+      if((f & a_USER_LOCK) && !(f & a_SESSIONS))
+         a_LOG(pamh, a_LOG_NOTICE,
+            a_XDG ": \"per_user_lock\" requires \"track_user_sessions\"");
+#endif
+   }else{
+      f = f_saved;
+
+      if(f & a_SKIP_XDG)
+         goto jok;
       goto jok; /* No longer used, session counting does not work */
+   }
 
    /* We need the user we go for */
    if((res = pam_get_item(pamh, PAM_USER, (void const**)&user)
@@ -113,79 +192,81 @@ a_xdg(int isopen, pam_handle_t *pamh, int flags, int argc, const char **argv){
    }
 
    if((pwp = getpwnam(user)) == NULL){
-      emsg = "host machine does not know about user";
+      emsg = "host does not know about user";
       errno = EINVAL;
       goto jerr;
    }
 
-   if(notroot && pwp->pw_uid == 0)
+   if((f & a_NOTROOT) && pwp->pw_uid == 0){
+      f |= a_SKIP_XDG;
       goto jok;
-
-   /* I admit all this is overly complicated and expensive */
-   if((cwdfd = open(a_RUNTIME_DIR_OUTER, (O_PATH | O_DIRECTORY | O_NOFOLLOW))
-         ) == -1){
-      emsg = "cannot obtain chdir(2) descriptor to " a_RUNTIME_DIR_OUTER;
-      goto jerr;
    }
 
-   /* We try create the base directory once as necessary */
-   /*if(isopen)*/{
+   /* Handle outer directory tree.  On *BSD outermost may not exist! */
+   for(/*f &= ~a_MPV,*/ dtp = a_dirtree; dtp->name != NULL;){
+      int e;
       gid_t oegid;
       mode_t oumask;
 
-      res = 0;
-      while(fstatat(cwdfd, a_RUNTIME_DIR_BASE, &st, AT_SYMLINK_NOFOLLOW
-            ) == -1){
-         if(res++ != 0 || errno != ENOENT){
-            emsg = "base directory " a_RUNTIME_DIR_OUTER "/" a_RUNTIME_DIR_BASE
-                  " not accessible";
-            goto jerr;
-         }
+      if((res = openat(cwdfd, dtp->name,
+               (a_O_SEARCH | O_DIRECTORY | O_NOFOLLOW))) != -1){
+         if(cwdfd != AT_FDCWD)
+            close(cwdfd); /* XXX error hdl */
+         cwdfd = res;
+         f &= ~a_MPV;
+         ++dtp;
+         continue;
+      }
 
-         oumask = umask(0000);
-         oegid = getegid();
-         setegid(0);
+      if(!isopen)
+         /* Someone removed the entire directory tree while sessions were open!
+          * Silently out!?! */
+         goto jok;
 
-         if(mkdirat(cwdfd, a_RUNTIME_DIR_BASE, a_RUNTIME_DIR_BASE_MODE
-               ) == -1 && errno != EEXIST){
-            emsg = "cannot create base directory "
+      /* We try create the directories once as necessary */
+      if((f & a_MPV) || errno != ENOENT){
+         emsg = "cannot obtain chdir(2) descriptor (within) tree "
+               a_RUNTIME_DIR_OUTER "/" a_RUNTIME_DIR_BASE;
+         goto jerr;
+      }
+      f |= a_MPV;
+
+      oumask = umask(0000);
+      oegid = getegid();
+      setegid(0);
+         res = mkdirat(cwdfd, dtp->name, dtp->mode);
+         e = (res == -1) ? errno : 0;
+      setegid(oegid);
+      umask(oumask);
+
+      if(res == -1){
+         if(e != EEXIST){
+            emsg = "cannot create directory (within) tree "
                   a_RUNTIME_DIR_OUTER "/" a_RUNTIME_DIR_BASE;
             goto jerr;
          }
-
-         setegid(oegid);
-         umask(oumask);
-      }
-      /* Not worth doing S_ISDIR(st.st_mode), O_DIRECTORY will bail next */
+      }else if(cwdfd == AT_FDCWD)
+         a_LOG(pamh, a_LOG_NOTICE,
+            a_XDG ": " a_RUNTIME_DIR_OUTER " did not exist, but should be "
+            "(a mount point of) volatile storage!");
    }
 
-   if((res = openat(cwdfd, a_RUNTIME_DIR_BASE,
-         (O_PATH | O_DIRECTORY | O_NOFOLLOW))) == -1){
-      emsg = "cannot obtain chdir(2) descriptor to " a_RUNTIME_DIR_OUTER "/"
-            a_RUNTIME_DIR_BASE;
-      goto jerr;
-   }
-   close(cwdfd);
-   cwdfd = res;
-
-   /* Turn to user management */
-   uidbuflen = snprintf(uidbuf, sizeof(uidbuf), "%lu",
+   /* Turn to user management; note this is the lockfile */
+   uidbuflen = snprintf(uidbuf, sizeof(uidbuf), ".%lu",
          (unsigned long)pwp->pw_uid);
 
    /* We create the per-user directory on isopen time as necessary */
-   for(res = 0;; ++res){
-      int nfd;
-
-      if((nfd = openat(cwdfd, uidbuf, (O_PATH | O_DIRECTORY | O_NOFOLLOW))
-            ) != -1){
-         close(cwdfd);
-         cwdfd = nfd;
+   for(f &= ~a_MPV;; f |= a_MPV){
+      if((res = openat(cwdfd, &uidbuf[1],
+            (a_O_SEARCH | O_DIRECTORY | O_NOFOLLOW))) != -1){
+         close(cwdfd); /* XXX error hdl */
+         cwdfd = res;
          break;
       }else{
          if(errno == ENOENT){
             if(!isopen)
                goto jok;
-            if(res != 0)
+            if(f & a_MPV)
                goto jeurd;
          }else{
 jeurd:
@@ -194,13 +275,13 @@ jeurd:
          }
       }
 
-      if(mkdirat(cwdfd, uidbuf, 0700) == -1 && errno != EEXIST){
+      if(mkdirat(cwdfd, &uidbuf[1], 0700) == -1 && errno != EEXIST){
          emsg = "cannot create per user XDG_RUNTIME_DIR";
          goto jerr;
       }
 
       /* Just chown it! */
-      if(fchownat(cwdfd, uidbuf, pwp->pw_uid, pwp->pw_gid,
+      if(fchownat(cwdfd, &uidbuf[1], pwp->pw_uid, pwp->pw_gid,
             AT_SYMLINK_NOFOLLOW) == -1){
          emsg = "cannot chown(2) per user XDG_RUNTIME_DIR";
          goto jerr;
@@ -208,7 +289,7 @@ jeurd:
    }
 
    /* When opening, we want to put environment variables, too */
-   /*if(isopen)*/{
+   if(isopen){
       char *cp;
 
       /* XDG_RUNTIME_DIR */
@@ -221,18 +302,20 @@ jeurd:
       memcpy(cp, a_RUNTIME_DIR_BASE, sizeof(a_RUNTIME_DIR_BASE) -1);
       cp += sizeof(a_RUNTIME_DIR_BASE) -1;
       *cp++ = '/';
-      memcpy(cp, uidbuf, uidbuflen +1);
+      memcpy(cp, &uidbuf[1], uidbuflen);
 
       if((res = pam_putenv(pamh, wbuf)) != PAM_SUCCESS)
          goto jepam;
 
-      /* And the rest */
-      if(!only_runtime){
-         struct adir{
+      /* And the rest unless disallowed */
+      if(!(f & a_RUNTIME)){
+         struct a_dir{
             char const *name;
             size_t len;
             char const *defval;
-         } const adirs[] = {
+         };
+
+         static struct a_dir const a_dirs[] = {
             {"XDG_DATA_HOME=", sizeof("XDG_DATA_HOME=") -1,
                a_XDG_DATA_HOME_DEF},
             {"XDG_CONFIG_HOME=", sizeof("XDG_CONFIG_HOME=") -1,
@@ -243,15 +326,16 @@ jeurd:
                a_XDG_CONFIG_DIRS_DEF},
             {"XDG_CACHE_HOME=", sizeof("XDG_CACHE_HOME=") -1,
                a_XDG_CACHE_HOME_DEF},
-            {NULL,0,NULL} /* xxx nelem */
-         }, *adp;
+            {NULL,0,NULL} /* XXX -> nelem/item/countof */
+         };
 
          char const *src;
+         struct a_dir const *adp;
          size_t i;
 
          i = strlen(pwp->pw_dir);
 
-         for(adp = adirs; adp->name != NULL; ++adp){
+         for(adp = a_dirs; adp->name != NULL; ++adp){
             cp = wbuf;
             memcpy(cp, adp->name, adp->len);
             cp += adp->len;
@@ -271,20 +355,23 @@ jeurd:
 jok:
    res = PAM_SUCCESS;
 jleave:
-   if(cwdfd != -1)
+   if(cwdfd != -1 && cwdfd != AT_FDCWD) /* >=0, but AT_FDCWD unspecified */
       close(cwdfd);
 
+   f_saved = f;
    return (res == PAM_SUCCESS) ? PAM_SUCCESS : PAM_SESSION_ERR;
 
 jerr:
-   pam_syslog(pamh, LOG_ERR, a_XDG ": user %s: %s: %s\n",
+   a_LOG(pamh, a_LOG_ERR, a_XDG ": user %s: %s: %s\n",
       user, emsg, strerror(errno));
+   f |= a_SKIP_XDG;
    res = PAM_SESSION_ERR;
    goto jleave;
 
 jepam:
-   pam_syslog(pamh, LOG_ERR, a_XDG ": user %s: PAM failure: %s\n",
+   a_LOG(pamh, a_LOG_ERR, a_XDG ": user %s: PAM failure: %s\n",
       user, pam_strerror(pamh, res));
+   f |= a_SKIP_XDG;
    goto jleave;
 }
 
@@ -305,7 +392,9 @@ pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv){
    (void)flags;
    (void)argc;
    (void)argv;
-   pam_syslog(pamh, LOG_NOTICE, "pam_sm_acct_mgmt not used by " a_XDG);
+
+   a_LOG(pamh, a_LOG_NOTICE, a_XDG ": pam_sm_acct_mgmt not used");
+
    return PAM_SERVICE_ERR;
 }
 
@@ -314,7 +403,9 @@ pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv){
    (void)flags;
    (void)argc;
    (void)argv;
-   pam_syslog(pamh, LOG_NOTICE, "pam_sm_setcred not used by " a_XDG);
+
+   a_LOG(pamh, a_LOG_NOTICE, a_XDG ": pam_sm_setcred not used");
+
    return PAM_SERVICE_ERR;
 }
 
@@ -323,7 +414,9 @@ pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv){
    (void)flags;
    (void)argc;
    (void)argv;
-   pam_syslog(pamh, LOG_NOTICE, "pam_sm_chauthtok not used by " a_XDG);
+
+   a_LOG(pamh, a_LOG_NOTICE, a_XDG ": pam_sm_chauthtok not used");
+
    return PAM_SERVICE_ERR;
 }
 
